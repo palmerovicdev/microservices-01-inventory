42beaa1cb87b34b01f47ce0735647af353bed229
palmerodev <victor.palmero@undostres.com.mx>
2023-12-11 00:53:41 -0500

    create microservices inventory

---
+++ src/main/java/org/suehay/microservicesinventory/MicroservicesInventoryApplication.java
@@ -0,0 +1,13 @@
+package org.suehay.microservicesinventory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicesInventoryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroservicesInventoryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/suehay/microservicesinventory/MicroservicesInventoryApplicationTests.java
@@ -0,0 +1,13 @@
+package org.suehay.microservicesinventory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicesInventoryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
